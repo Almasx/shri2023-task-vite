060eb7946e12fe99d0186d0ba931e25ad4be8941
Almasx <saparovalmas0@gmail.com>
2023-07-09 22:33:58 +0600

    vite

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+import { Header, Main } from './App'
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <Header />
+    <Main />
+  </>
+)
