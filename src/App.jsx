3a6f7b71e6f0918d536e94d1df7f951822d60446
Almasx <saparovalmas0@gmail.com>
2023-07-09 23:52:04 +0600

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -340,7 +340,7 @@ function DevicesSlider(props) {
 
      <div className="section__panel-wrapper" ref={ref}>
      {TABS_KEYS.map(key =>
-         <div key={key} role="tabpanel" sliderRef={sliderRef}
+         <div key={key} role="tabpanel" ref={sliderRef}
          className={'section__panel' + (key === activeTab ? '' : ' section__panel_hidden')} aria-hidden={key === activeTab ? 'false' : 'true'} id={`panel_${key}`} aria-labelledby={`tab_${key}`}>
              <ul className="section__panel-list">
                  {TABS[key].items.map((item, index) =>
